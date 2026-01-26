47cccf9d9955f1eaad767dfd29c0406f70579c0f
nathan <nala@uci.edu>
2026-01-26 11:08:43 -0800

    added parameters to the card class

--- course-project/src/Components/Card.jsx
+++ course-project/src/Components/Card.jsx
@@ -1,12 +1,14 @@
 
-export default function Card() {
+
+//Card class to display class information
+export default function Card({className, description}) {
     return (
         <div className="flex flex-col bg-blue-300 rounded-lg shadow-md max-w-sm m-4">
             <div className="text-lg text-white p-2 text-center bg-blue-400 rounded-t-lg">
-                Class Name
+                {className}
             </div>
-            <div className="text-lg text-white p-4">
-                Class Description:
+            <div className="text-lg text-white p-4 rounded-b-lg">
+                {description}
             </div>
         </div>
     );
