c9fd9c47d0b5bea123e162dc2e665c42d5fa5fbf
nathan <nala@uci.edu>
2026-01-26 21:48:06 -0800

    added a hover animation to the cards

--- course-project/src/Components/Card.jsx
+++ course-project/src/Components/Card.jsx
@@ -11,7 +11,7 @@ export default function Card({className, description}) {
     }
 
     return (
-        <div className="flex flex-col bg-blue-300 rounded-lg shadow-md max-w-sm m-4">
+        <div className="flex flex-col bg-blue-300 rounded-lg shadow-md max-w-sm transition-all hover:shadow-lg  hover:scale-101">
             <div className="text-lg text-white p-2 text-center bg-blue-400 rounded-t-lg">
                 {className}
             </div>
