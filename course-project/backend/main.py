f4bc8d0edcac3956629e881df7081d99dc7b8a97
nathan <nala@uci.edu>
2026-01-28 11:39:48 -0800

    created the login form for the website and connected it to the backend so it check to see if the username exsists and errors if it doesnt

--- course-project/backend/main.py
+++ course-project/backend/main.py
@@ -1,9 +1,11 @@
 # added a bloilerpalte fastapi server as a starting point created with gemini ai
 
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Cookie
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from fastapi.exceptions import HTTPException
 
 # Load environment variables from .env
 load_dotenv()
@@ -13,7 +15,9 @@ app = FastAPI(title="FastAPI Backend")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173"
+    ],  # Specified origin is required for credentials
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -47,6 +51,24 @@ async def api_class_info():
         "status": "ok",
         "message": "Class info is running smoothly",
     }
+
+
+class LoginRequest(BaseModel):
+    username: str
+
+
+@app.post("/api/login")
+async def api_login(request: LoginRequest):
+    usernames = ["nathan", "alyssia", "peter"]
+    if request.username in usernames:
+        return {
+            "status": "ok",
+            "message": f"Login is running smoothly, user: {request.username} has been seen",
+            "userid": request.username,
+        }
+    else:
+        # This sends a 404 status code to the frontend
+        raise HTTPException(status_code=404, detail="User not found")
 
 
 if __name__ == "__main__":
--- course-project/src/Components/NavBar.jsx
+++ course-project/src/Components/NavBar.jsx
@@ -3,8 +3,9 @@
 export default function NavBar() {
     return (
         // nav bar, will add links and stuff with react router later
-        <nav className="flex p-4 bg-blue-500 justify-between">
+        <nav className="flex p-4 bg-blue-500 justify-left">
             <div className="p-2 text-2xl text-white">Course Recommender</div>
+            <div className="p-2 text-2xl text-white">Login</div>
             <div className="p-2 text-2xl text-white">Search</div>
         </nav>
     );
