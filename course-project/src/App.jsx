af826b179653ca5232e7f4cb265cd672c90ec4a3
nathan <nala@uci.edu>
2026-01-28 17:59:39 -0800

    Cleaned up the UI of the website and added icons. Added esc key handling for the login page and added the start of the settings page

--- course-project/src/App.jsx
+++ course-project/src/App.jsx
@@ -1,16 +1,19 @@
 import NavBar from './Components/NavBar'
 import ClassCardCollection from './Components/ClassCardCollection'
+import UserSettingsPage from './Pages/UserSettingsPage'
 import LoginPage from './Pages/LoginPage'
 import { useState } from 'react'
 
 function App() {
 
   const [loginPrompt, setLogin] = useState(false);
+  const [settingsPrompt, setSettings] = useState(false);
 
   return (
     <>
-      <NavBar setLogin={setLogin} />
+      <NavBar setLogin={setLogin} setSettings={setSettings} />
       {loginPrompt ? <LoginPage setLogin={setLogin} /> : <ClassCardCollection />}
+      {settingsPrompt ? <UserSettingsPage setSettings={setSettings} /> : null}
     </>
   )
 }
