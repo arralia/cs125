59cc833104bd26b181d77426b9e3fe60adc12522
nathan <nala@uci.edu>
2026-01-29 12:29:59 -0800

    Made the sliders for the form so the user can input their skill levels and cleaned up some code

--- course-project/src/Components/ClassCardCollection.jsx
+++ course-project/src/Components/ClassCardCollection.jsx
@@ -1,25 +1,29 @@
 import Card from "./Card";
 import useApiGet from "./useApiGet";
-import readCookie from "./readCookie";
 import { useEffect } from "react";
+import ReadCookie from "./ReadCookie";
 
 export default function ClassCardCollection() {
   // Handle the cookie logic cleanly
-  const userId = readCookie("user_id") || null;
+  const userId = ReadCookie("user_id") || null;
 
   // Destructure the values from your hook
-  const { execute, loading, response : classes} = useApiGet({
-    api: "/api/classInfo"
+  const {
+    execute,
+    loading,
+    response: classes,
+  } = useApiGet({
+    api: "/api/classInfo",
   });
 
   // Use useEffect to trigger the fetch once
   useEffect(() => {
-    execute({ params: { userid: userId } }); 
+    execute({ params: { userid: userId } });
     // We pass an empty array [] below so this only runs ONCE on mount
   }, []);
 
   return (
-    <div className="flex flex-col justify-left p-4 gap-4">
+    <div className="flex flex-col justify-center p-4 gap-4">
       {classes?.data?.map((item, index) => (
         <Card
           key={index} // Keys help React track list items
