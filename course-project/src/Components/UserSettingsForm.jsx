00710c97228177d91f2eac4fbaba8e8dc8cf187c
nathan <nala@uci.edu>
2026-02-07 23:10:11 -0800

    Implement user settings form with skill and class management, API integration, and minor backend cleanup.

--- course-project/src/Components/UserSettingsForm.jsx
+++ course-project/src/Components/UserSettingsForm.jsx
@@ -4,8 +4,11 @@
 // Quarters left till graditiaton
 
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import useApiPost from "./useApiPost";
+import useApiGet from "./useApiGet";
 
-export default function UserSettingsForm() {
+export default function UserSettingsForm({ setSettings }) {
   // These are the 3 tools you'll use 90% of the time
   const {
     register,
@@ -14,36 +17,65 @@ export default function UserSettingsForm() {
   } = useForm();
 
   const onSubmit = (data) => {
+    setSettings(false);
     console.log("Clean JSON data:", data);
   };
 
+  const [settingsPage, setSettingsPage] = useState("skills");
+
+  // this gets the class info when the settings page first comes up
+  // so we can populate the drop down menu for the user to select
+  const { execute } = useApiGet({
+    api: "/api/classInfo",
+  });
+
+  useEffect(() => {
+    const classList = execute();
+  }, []);
+
   const skills = [
     { name: "Math", value: 0 },
-    { name: "Physics", value: 0 },
-    { name: "Chemistry", value: 0 },
-    { name: "Biology", value: 0 },
-    { name: "Computer Science", value: 0 },
+    { name: "Algorithms", value: 0 },
+    { name: "Data Structures", value: 0 },
+    { name: "Programming", value: 0 },
+    { name: "Recursion", value: 0 },
   ];
-
-
 
   return (
     <div>
       <h2 className="text-3xl font-bold mb-2">User Settings</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-        <h3 className="text-xl font-bold">Skills</h3> 
+        <div className="flex">
+          <button className="text-xl font-bold outline px-3 rounded-l-md hover:text-blue-700 hover:" onClick={() => {setSettingsPage("Skills")}}>
+            Skills
+          </button>
+          <button className="text-xl font-bold outline px-3 rounded-r-md hover:text-blue-700" onClick={() => {setSettingsPage("Classes")}}>
+            Classes
+          </button>
+        </div>
         {/* map through the skills */}
         {skills.map((skill) => (
-          <div className="flex gap-4 justify-between">
+          <div key={skill.name} className="flex gap-4 justify-between">
             <label>{skill.name}</label>
             <div className="flex gap-2 justify-center items-center">
               <label>1</label>
-                  <input id={skill.name} type="range"min="1" max="5" step="1" {...register(skill.name)} />
+              <input
+                id={skill.name}
+                type="range"
+                min="1"
+                max="5"
+                step="1"
+                {...register(skill.name)}
+              />
               <label>5</label>
             </div>
           </div>
         ))}
-        <input type="submit" value="Save" className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 cursor-pointer" />
+        <input
+          type="submit"
+          value="Save"
+          className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 cursor-pointer"
+        />
       </form>
     </div>
   );
